7274b67f8b381081bca3141d744d5710fbb8a6ee
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-22 15:48:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,3 +4,11 @@ class EmailParser
   def initialize(csv_emails)
     @csv_emails = csv_emails
   end 
+  
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
